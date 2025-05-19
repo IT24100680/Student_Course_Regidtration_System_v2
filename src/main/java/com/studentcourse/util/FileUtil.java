fa999480036a827089725874972414abb2f72b61
IT24100610 <it24100610@my.sliit.lk>
2025-05-19 20:30:01 +0530

    System.err.println("Error writing to payment file: " + e.getMessage());

--- src/main/java/com/studentcourse/util/FileUtil.java
+++ src/main/java/com/studentcourse/util/FileUtil.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class FileUtil {
-    // Relative path that will work in deployed environment
+
     private static final String FILE_NAME = "payments.txt";
 
     public static List<String> readFile() {
@@ -32,18 +32,17 @@ public class FileUtil {
     }
 
     private static String getFilePath() {
-        // For development environment (when running from IDE)
         String devPath = System.getProperty("user.dir") + File.separator + "src" +
                 File.separator + "main" + File.separator + "resources" +
                 File.separator + FILE_NAME;
 
-        // For deployed environment (in Tomcat)
+
         String deployedPath = System.getProperty("catalina.base") + File.separator +
                 "webapps" + File.separator + "your-app-name" +
                 File.separator + "WEB-INF" + File.separator + "classes" +
                 File.separator + FILE_NAME;
 
-        // Check which path exists and use it
+
         File devFile = new File(devPath);
         if (devFile.exists()) {
             return devPath;
@@ -54,14 +53,13 @@ public class FileUtil {
             return deployedPath;
         }
 
-        // If file doesn't exist, create it in the deployed location
         try {
             deployedFile.getParentFile().mkdirs();
             deployedFile.createNewFile();
             return deployedPath;
         } catch (IOException e) {
             System.err.println("Could not create payment file: " + e.getMessage());
-            return devPath; // fallback
+            return devPath;
         }
     }
 }
