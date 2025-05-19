1d7ef99d360c423aa2b3bba1d9ec332229835ad7
IT24100610 <it24100610@my.sliit.lk>
2025-05-19 19:20:16 +0530

    I Changed attribute(studenID)

--- src/main/java/com/studentcourse/model/Payment.java
+++ src/main/java/com/studentcourse/model/Payment.java
@@ -5,26 +5,27 @@ import java.io.Serializable;
 public class Payment implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String studentId;
+    private String studentID;
     private double amount;
     private String method;
     private String date;
 
-    // Constructor
-    public Payment(String studentId, double amount, String method, String date) {
-        this.studentId = studentId;
+    public Payment(String studentID, double amount, String method, String date) {
+        this.studentID = studentID;
         this.amount = amount;
         this.method = method;
         this.date = date;
     }
 
-    // Getters and Setters
-    public String getStudentId() {
-        return studentId;
+
+    public String getStudentID() {
+
+        return studentID;
     }
 
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
+    public void setStudentID(String studentID) {
+
+        this.studentID = studentID;
     }
 
     public double getAmount() {
@@ -32,32 +33,35 @@ public class Payment implements Serializable {
     }
 
     public void setAmount(double amount) {
+
         this.amount = amount;
     }
 
     public String getMethod() {
+
         return method;
     }
 
     public void setMethod(String method) {
+
         this.method = method;
     }
 
     public String getDate() {
+
         return date;
     }
 
     public void setDate(String date) {
+
         this.date = date;
     }
 
-    // toString method for file storage
     @Override
     public String toString() {
-        return studentId + "," + amount + "," + method + "," + date;
+        return studentID + "," + amount + "," + method + "," + date;
     }
 
-    // Static method to parse a CSV line into a Payment object
     public static Payment fromCSV(String csvLine) {
         String[] fields = csvLine.split(",");
         return new Payment(fields[0], Double.parseDouble(fields[1]), fields[2], fields[3]);
